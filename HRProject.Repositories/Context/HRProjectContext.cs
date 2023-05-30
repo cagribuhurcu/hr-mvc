0306f6c3aea893daa42cfca3ff22e7c9967bf6d1
Furkan Bayar <furkan.byr@outlook.com>
2023-05-30 13:50:23 +0300

    Add project files.

---
+++ HRProject.Repositories/Context/HRProjectContext.cs
@@ -0,0 +1,29 @@
+﻿using HRProject.Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.Repositories.Context
+{
+    public class HRProjectContext : DbContext 
+    {
+        public HRProjectContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+
+        //Azure Database Bağlantısı İçin
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=tcp:ikkaynak.database.windows.net,1433;Initial Catalog=HRProjectDatabase;Persist Security Info=False;User ID=ikkaynak;Password=admin123.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+
+        //DBSetler bu kısımın altında olacak
+        public DbSet<User> Users { get; set; }
+    }
+}
