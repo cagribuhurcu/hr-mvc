773dcd7b3599d011d283912107ccbdf70f9fc66c
Furkan Bayar <furkan.byr@outlook.com>
2023-05-31 16:01:21 +0300

    son değişiklik

--- HRProject.Entities/Entities/User.cs
+++ HRProject.Entities/Entities/User.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HRProject.Entities.Enums;
+using Microsoft.AspNetCore.Identity;
 
 namespace HRProject.Entities.Entities
 {
-    public class User : BaseEntity
+    public class User : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string? SecondLastName { get; set; }
+        public string BirthPlace { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string IdentificationNumber { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime? QuitDate { get; set; }
+        public string EmailAddress
+        {
+            get { return $"{FirstName}.{LastName}@bilgeadam.com"; }
+        }
+
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; } = true;
+        public Departments Department { get; set; }
+        public Job Job { get; set; }
+
     }
 }
