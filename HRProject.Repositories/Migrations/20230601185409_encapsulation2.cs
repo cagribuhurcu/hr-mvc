76585f52cbecc5cea8a345a1d0b3ee74eeef37b3
Furkan Bayar <furkan.byr@outlook.com>
2023-06-01 22:09:27 +0300

    encapsulation denemeler yaptım geri çekiyorum fakat olmadığı için mastera eklemem gerekiyor

---
+++ HRProject.Repositories/Migrations/20230601185409_encapsulation2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRProject.Repositories.Migrations
+{
+    public partial class encapsulation2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
