c73c654459602d012f62a67e62fd81b72ff6954d
NadireCobanoglu <nadirecobanoglu@gmail.com>
2023-06-01 11:22:35 +0300

    Service katmanı eklendi

---
+++ HRProject.Service/Abstract/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿using HRProject.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.Service.Abstract
+{
+    public interface IGenericService<T> where T: BaseEntity
+    {
+
+
+    }
+}
