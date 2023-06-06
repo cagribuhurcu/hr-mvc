8f9185bce609b97bfcecdb460a5fb2a7f50dcbbe
cagribuhurcu <cagribuhurcu@gmail.com>
2023-06-06 16:47:09 +0300

    companyAPI => getAll, getById

---
+++ HRProject.API/Controllers/CompanyController.cs
@@ -0,0 +1,36 @@
+﻿using HRProject.Entities.Entities;
+using HRProject.Service.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRProject.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CompanyController : Controller
+    {
+        private readonly IGenericService<Company> service;
+
+        public CompanyController(IGenericService<Company> service)
+        {
+            this.service = service;
+        }
+
+        //Listele
+
+        [HttpGet]
+        public IActionResult GetAllCompanies()
+        {
+            var companies = service.GetAll();
+            return Ok(companies);
+        }
+
+        //Company Özet Bilgileri
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var company = service.GetById(id);
+            return Ok(company);
+        }
+    }
+}
