937d76f4b02391c0144c514790c35a18e69bf7a0
NadireCobanoglu <nadirecobanoglu@gmail.com>
2023-06-06 12:13:00 +0300

    Company Class + encapsulation yapıldı

--- HRProject.Entities/Enums/Departments.cs
+++ HRProject.Entities/Enums/Departments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace HRProject.Entities.Enums
 {
     public enum Departments
     {
-        HumanResources=1,//İnsan Kaynakları
+        [Description("Human Resources")]
+        HumanResources =1,//İnsan Kaynakları
         Marketing=2,//Pazarlama
         Finance=3,//Finans
         Sales=4,//Satış
-        InformationSystems=5//Bilişim Sistemlerii
+        [Description("Information Systems")]
+        InformationSystems =5//Bilişim Sistemlerii
     }
 }
