204301a9a38f3ad31edece0f4e1c5449575bedc0
Bertac <berna.demirsoyy@gmail.com>
2023-06-08 00:17:52 +0300

    taxvalue and mersis validation added

---
+++ HRProject.Entities/Validation/MersisValidation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HRProject.Entities.Validation.TaxNumberValidation;
+
+namespace HRProject.Entities.Validation
+{
+    public static class MersisValidation
+    {
+        public static bool IsMersisNoValid(string mersisNo,string vergiNo)
+        {
+            if (mersisNo.Length != 16)
+            {
+                return false;
+            }
+            else if(mersisNo.Length==16)
+            {
+                if (!mersisNo.StartsWith("0"))
+                {
+                    return false;
+
+                }
+                else
+                {
+                    vergiNo = mersisNo.Substring(1, 10);
+                    if (!IsTaxNumber(vergiNo))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (mersisNo.EndsWith("00015") || mersisNo.EndsWith("00016") || mersisNo.EndsWith("00017"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ HRProject.Entities/Validation/TaxNumberValidation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRProject.Entities.Validation
+{
+    public static class TaxNumberValidation
+    {
+        public static bool IsTaxNumber(string txn)
+        {
+            int tmp;
+            int sum = 0;
+            if (txn != null && txn.Length == 10 && IsInt(txn))
+            {
+                int lastDigit = int.Parse(txn[9].ToString());
+                for (int i = 0; i < 9; i++)
+                {
+                    int digit = int.Parse(txn[i].ToString());
+                    tmp = (digit + 10 - (i + 1)) % 10;
+                    sum = (int)((tmp == 9) ? sum + tmp : sum + ((tmp * (Math.Pow(2, 10 - (i + 1)))) % 9));
+                }
+                return lastDigit == (10 - (sum % 10)) % 10;
+            }
+            return false;
+        }
+
+        private static bool IsInt(string s)
+        {
+            for (int a = 0; a < s.Length; a++)
+            {
+                if (a == 0 && s[a] == '-') continue;
+                if (!char.IsDigit(s[a])) return false;
+            }
+            return true;
+        }
+    }
+}
