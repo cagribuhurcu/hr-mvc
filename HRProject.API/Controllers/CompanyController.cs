b9605711ca273580ca9c86da39e84df75c84bfc2
Furkan Bayar <furkan.byr@outlook.com>
2023-06-07 16:41:49 +0300

    companyadeed

---
+++ HRProject.API/Controllers/CompanyController.cs
@@ -0,0 +1,54 @@
+﻿using HRProject.Entities.Entities;
+using HRProject.Service.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRProject.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CompanyController : Controller
+    {
+        private readonly IGenericService<Company> service;
+
+        public CompanyController(IGenericService<Company> service)
+        {
+            this.service = service;
+        }
+
+        //Listele
+
+        [HttpGet]
+        public IActionResult GetAllCompanies()
+        {
+            var companies = service.GetAll();
+            return Ok(companies);
+        }
+
+        //Company Özet Bilgileri
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var company = service.GetById(id);
+            return Ok(company);
+        }
+
+        // Company Add
+
+        [HttpPost]
+        public IActionResult CreateCompany([FromBody] Company newCompany)
+        {
+            var company = service.GetByDefault(x => x.CompanyName == newCompany.CompanyName);
+            if (company is not null)
+            {
+                return BadRequest("The company already exists");
+            }
+            else
+            {
+                service.Add(newCompany);
+                return Ok("Company successfully added");
+            }
+        }
+    }
+}
