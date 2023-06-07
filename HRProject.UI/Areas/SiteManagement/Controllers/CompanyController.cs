34b2b1ad9184d89f099e6aea5ddfee8c0a4b5286
Bertac <berna.demirsoyy@gmail.com>
2023-06-07 11:53:56 +0300

    UI CompanyControl eklendi

---
+++ HRProject.UI/Areas/SiteManagement/Controllers/CompanyController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HRProject.Entities.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRProject.UI.Areas.SiteManagement.Controllers
+{
+    [Area("SiteManagement"), Authorize(Roles = "SiteManager")]
+    public class CompanyController : Controller
+    {
+        string baseURL = "https://hrprojectapi20230605125226.azurewebsites.net";
+        private readonly IMapper _mapper;
+
+        public CompanyController(IMapper mapper)
+        {
+            _mapper = mapper;
+          
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
