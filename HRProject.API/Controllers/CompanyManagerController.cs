37b4dba5c77cb7ffdc1c2227ffc8e623fd2991d6
Furkan Bayar <furkan.byr@outlook.com>
2023-06-13 11:55:21 +0300

    ufaktefek

---
+++ HRProject.API/Controllers/CompanyManagerController.cs
@@ -0,0 +1,35 @@
+﻿//using HRProject.Repositories.Abstract;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace HRProject.API.Controllers
+//{
+//    [Route("api/[controller]/[action]")]
+//    [ApiController]
+//    public class CompanyManagerController : ControllerBase
+//    {
+//        private readonly IGenericRepository<CompanyManager> service;
+//        public CompanyManagerController(IGenericRepository<CompanyManager> service)
+//        {
+//            this.service = service;
+//        }
+
+//        //Listelemek için kullanılan action 
+
+//        [HttpGet]
+//        public IActionResult GetAllCompanyManagers()
+//        {
+//            var companyManagers = service.GetAll();
+//            return Ok (companyManagers);
+//        }
+
+//        //Şirket Yöneticisi Özet Bilgileri
+
+//        [HttpGet("{id}")]
+//        public IActionResult GetCompanyManagerById(int id)
+//        {
+//            var companyManager = service.GetById(id);
+//            return Ok(companyManager);
+//        }
+//    }
+//}
---
+++ HRProject.UI/Areas/CompanyManager/Controllers/CompanyManagerController.cs
@@ -0,0 +1,166 @@
+﻿//using AutoMapper;
+//using HRProject.Entities.Entities;
+//using HRProject.Entities.Enums;
+//using HRProject.UI.Areas.SiteManagement.Models;
+//using HRProject.UI.Models.DTOs;
+//using Microsoft.AspNetCore.Authentication;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json.Linq;
+//using Newtonsoft.Json;
+//using System.Security.Claims;
+//using System.Text;
+
+//namespace HRProject.UI.Areas.CompanyManager.Controllers
+//{
+//    [Area("CompanyManager"), Authorize(Roles = "CompanyManager")]
+//    public class CompanyManagerController : Controller
+//    {
+//        private readonly IMapper _mapper;
+//        private readonly IWebHostEnvironment environment;
+//        string baseURL = "https://localhost:7127";
+
+//        public CompanyManagerController(IMapper mapper, IWebHostEnvironment environment)
+//        {
+//            _mapper = mapper;
+//            this.environment = environment;
+//        }
+
+//        public async Task<IActionResult> Index()
+//        {
+//            List<CompanyManager> companyManagers = new List<CompanyManager>();
+
+//            using (var httpClient = new HttpClient())
+//            {
+//                using (var cevap = await httpClient.GetAsync($"{baseURL}/api/User/GetAllCompanyManagers"))
+//                {
+//                    string apiCevap = await cevap.Content.ReadAsStringAsync();
+//                    companyManagers = JsonConvert.DeserializeObject<List<CompanyManager>>(apiCevap);
+//                }
+//            }
+//            var newLoginClaim = new Claim("PhotoUrl", companyManagers[0].PhotoURL);
+
+//            var currentCompanyManager = HttpContext.companyManager.Identity as ClaimsIdentity;
+//            var loginClaim = currentCompanyManager.FindFirst("PhotoUrl");
+//            if (loginClaim != null)
+//            {
+//                currentCompanyManager.RemoveClaim(loginClaim);
+//                currentCompanyManager.AddClaim(newLoginClaim);
+//            }
+//            var newPrincipal = new ClaimsPrincipal(currentCompanyManager);
+//            await HttpContext.SignInAsync(newPrincipal);
+//            return View(companyManagers);
+//        }
+
+
+//        [HttpGet]
+//        public async Task<IActionResult> Detail(int id)
+//        {
+//            User user = new User();
+//            using (var httpClient = new HttpClient())
+//            {
+//                using (var cevap = await httpClient.GetAsync($"{baseURL}/api/User/GetUserById/{id}"))
+//                {
+//                    string apiCevap = await cevap.Content.ReadAsStringAsync();
+//                    user = JsonConvert.DeserializeObject<List<User>>(apiCevap)[0];
+//                }
+//            }
+//            return Json(user);
+//        }
+//        static User updateduser;
+
+//        [HttpGet]
+//        public async Task<IActionResult> UpdateUser(int id)
+//        {
+
+//            using (var httpClient = new HttpClient())
+//            {
+//                using (var cevap = await httpClient.GetAsync($"{baseURL}/api/User/GetUserById/{id}"))
+//                {
+//                    string apiCevap = await cevap.Content.ReadAsStringAsync();
+//                    updateduser = JsonConvert.DeserializeObject<List<User>>(apiCevap)[0];
+//                }
+//            }
+
+//            UpdateUserVM vm = _mapper.Map<UpdateUserVM>(updateduser);
+
+//            return View(vm);
+//        }
+
+//        [HttpPost]
+//        public async Task<IActionResult> UpdateUser(UpdateUserVM uservm, List<IFormFile> files)
+//        {
+
+
+//            if (files.Count == 0) //Foto seçilemez ise
+//            {
+//                uservm.PhotoUrl = updateduser.PhotoURL;
+//            }
+//            else
+//            {
+
+//                string returnedMessaage = Upload.ImageUpload(files, environment, out bool imgresult);
+
+//                if (imgresult)
+//                {
+//                    uservm.PhotoUrl = returnedMessaage;//Eğer ImageUpload'dan fırlatılan değer true ise returnedMessage bana foto url'i döndürcek
+
+//                }
+//                else
+//                {
+//                    uservm.PhotoUrl = "/Uploads/ef2fefcf_0dbb_4239_8b28_7f87983acf87.jpeg";
+//                    ViewBag.PhotoMessage = returnedMessaage;
+//                    ModelState.AddModelError("", ViewBag.PhotoMessage);
+
+//                }
+//            }
+
+
+//            using (var httpClient = new HttpClient())
+//            {
+//                updateduser.PhotoURL = uservm.PhotoUrl;
+//                updateduser.Address = uservm.Address;
+//                updateduser.PhoneNumber = uservm.PhoneNumber;
+//                StringContent content = new StringContent(JsonConvert.SerializeObject(updateduser), Encoding.UTF8, "application/json");
+
+//                using (var response = await httpClient.PutAsync($"{baseURL}/api/User/UpdateUser/{updateduser.ID}", content))
+//                {
+
+//                    if (!response.IsSuccessStatusCode)
+//                    {
+//                        var jsonResponse = await response.Content.ReadAsStringAsync();
+//                        var errorResponse = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+
+//                        string errorMessage = "";
+//                        if (errorResponse.errors != null)
+//                        {
+//                            var errors = errorResponse.errors;
+//                            foreach (var error in errors)
+//                            {
+//                                var errorMessages = (JArray)error.Value;
+//                                foreach (var errorMessageToken in errorMessages)
+//                                {
+//                                    errorMessage += errorMessageToken.ToString() + "\n";
+//                                }
+//                            }
+//                        }
+//                        ModelState.AddModelError("", errorMessage); // Hata mesajını ModelState'e ekleyin
+//                        //var allmasseges = ModelState.ToList();
+//                        ViewData["allmasseges"] = ModelState.ToList();
+//                        return View(uservm);
+//                    }
+
+//                    if (!ModelState.IsValid)
+//                    {
+//                        ViewData["allmasseges"] = ModelState.ToList();
+//                        return View(uservm);
+//                    }
+
+
+//                }
+//            }
+
+//            return RedirectToAction("Index");
+//        }
+//    }
+//}
