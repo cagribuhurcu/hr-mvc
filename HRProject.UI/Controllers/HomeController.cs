4b7b9105657ba30ffb304f46844d766bc5cfe31c
cagribuhurcu <cagribuhurcu@gmail.com>
2023-06-19 14:54:41 +0300

    git

--- HRProject.UI/Controllers/HomeController.cs
+++ HRProject.UI/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace HRProject.UI.Controllers
     public class HomeController : Controller
     {
 
-        string baseURL = "https://lo";
+        string baseURL = "https://localhost:7127/";
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
